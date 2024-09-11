c4883d6168254a2f3b54fa3e48bb785bbd54104d
shemanto27 <shemantosharkarofficial@gmail.com>
2024-09-11 23:56:30 +0600

    doing

---
+++ assignment.js
@@ -0,0 +1,27 @@
+/*functionsignature/sample*/
+functioncalculateTax(income,expenses){
+    //Youhavetowriteyourcodehere
+    }
+   
+
+ /*functionsignature/sample*/
+ functionsendNoti cation(email){
+    //Youhavetowriteyourcodehere
+    }
+
+
+/*functionsignature/sample*/
+functioncheckDigitsInName(name){
+    //writeyourcodehere
+    }
+
+
+/*functionsignature/sample*/
+functioncalculateFinalScore(obj){
+    //writeyourcodehere
+    }
+
+/*functionsignature/sample*/
+functionwaitingTime(waitingTimes ,serialNumber){
+    //Youhavetowriteyourcodehere
+    }
